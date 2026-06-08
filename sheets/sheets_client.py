c41ee6be422ace873602227c09fa1b9863fb3cce
yashdeashpande09 <yashdeashpande09@gmail.com>
2026-06-08 23:16:20 +0530

    Update sheets_client.py

--- sheets/sheets_client.py
+++ sheets/sheets_client.py
@@ -1,18 +1,18 @@
+import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-
+@st.cache_resource
 def get_sheet_client():
+
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = Credentials.from_service_account_file(
-        "credentials/service_account.json",
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
         scopes=scopes
     )
 
-    client = gspread.authorize(creds)
-
-    return client
+    return gspread.authorize(creds)
